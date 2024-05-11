59951ef52381ea623d1d98137cf9d3e212b4be26
habib motavassel <habib.motavassel@gmail.com>
2024-05-11 16:35:46 +0200

    To add Like button to product cards

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { GlobalContext } from "../../context/Context";
 import Button from "../button/Button";
+import Like from "../Like/Like";
 import './card.scss'
 import Image from "../Image/Image";
 
-const ProductCard = ({children}) => {
+const ProductCard = ({ children }) => {
 
     const { cartItems, handleCartItems } = useContext(GlobalContext);
 
@@ -15,10 +16,13 @@ const ProductCard = ({children}) => {
     return (
         <div className="product-card">
             <div className='product-card--frame'>
-                <Image src={children.image} alt='' size="small"/>
+                <Image src={children.image} alt='' size="small" />
             </div>
             <Link to={`./product/${children.id}`}><h4>{children.title}</h4></Link>
-            <Button type="primary" onClick={() => handleCartItems({ type: "ADD", product: children })}>{itemIndex === -1 ? "Add to Cart" : "Added"}</Button>
+            <div className="product-card--action">
+                <Button type="primary" onClick={() => handleCartItems({ type: "ADD", product: children })}>{itemIndex === -1 ? "Add to Cart" : "Added"}</Button>
+                <Like onChange={() => console.log('Added to favorite list')}/>
+            </div>
         </div>
     )
 }
