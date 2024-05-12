886df88c1d259ec8e7e104fa6a375a76d7d6c519
habib motavassel <habib.motavassel@gmail.com>
2024-05-12 16:16:12 +0200

    Fix missing onToggle bug using a noop function

--- src/context/Toggle/Toggle.jsx
+++ src/context/Toggle/Toggle.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useRef, useState } from 'react'
 
 export const ToggleContext = createContext(null);
 
-const Toggle = ({ children, onToggle }) => {
+const Toggle = ({ children, onToggle = () => {}}) => {
 
     const [on, setOn] = useState(false);
     const firstRender = useRef(true)
