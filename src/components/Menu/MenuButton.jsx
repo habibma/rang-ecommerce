9d238c18c765b27183ed621703cb125c75913050
habib motavassel <habib.motavassel@gmail.com>
2024-05-26 17:57:23 +0200

    To get MenuButton to accept shape attr

--- src/components/Menu/MenuButton.jsx
+++ src/components/Menu/MenuButton.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import Button from '../button/Button'
 import { MenuContext } from '../../context/MenuContext'
 
-const MenuButton = ({ children }) => {
+const MenuButton = ({ children, shape }) => {
 
     const { toggleOpen, focused } = useContext(MenuContext);
 
@@ -12,6 +12,9 @@ const MenuButton = ({ children }) => {
             onBlur={!focused && open ? toggleOpen : undefined}
         >
             {children}
+            {shape === 'arrow' && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>}
         </Button>
     )
 }
--- src/components/pages/Products/Products.jsx
+++ src/components/pages/Products/Products.jsx
@@ -12,29 +12,8 @@ const Products = () => {
   return (
     <>
       <div className="sort-bar">
-        {/* <label htmlFor="categories">
-          Categories:
-        </label>
-        <select id="categories" className="category" value={select} onChange={({ target }) => setSelect(target.value)} >
-          <option value="" defaultValue>All</option>
-          <option value="electronics">electronics</option>
-          <option value="jewelery">jewelery</option>
-          <option value="men's clothing">men's clothing</option>
-          <option value="women's clothing">women's clothing</option>
-        </select> */}
-        {/* <select defaultValue="">
-                    <option>Price</option>
-                </select> */}
-        <label htmlFor="categories">
-          Categories:
-        </label>
         <Menu id="categories" onOpen={() => console.log("price clicked!")} onChange={setSelect}>
-          <Menu.Button>
-            {select || 'Categories'}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-            </svg>
-          </Menu.Button>
+          <Menu.Button shape='arrow'>{select || 'Categories'}</Menu.Button>
           <Menu.Dropdown>
             <Menu.Item value="electronics">Electronics</Menu.Item>
             <Menu.Item value="jewelery">Jewelery</Menu.Item>
