1668d014e108afa92799677c8e26a8c6b14e9e24
habib motavassel <habib.motavassel@gmail.com>
2024-07-01 10:55:17 +0200

    To make upload more secure

--- src/components/profilePicture/ProfilePicture.jsx
+++ src/components/profilePicture/ProfilePicture.jsx
@@ -10,11 +10,23 @@ import { upload } from '../../api'
 const ProfilePicture = () => {
 
     const currentUser = useAuth()
-    const [photo, setPhoto] = useState(null)
+    const [photo, setPhoto] = useState()
+    const [error, setError] = useState('')
     const [loading, setLoading] = useState('idle')
 
     const handleChange = ({ target }) => {
-        if (target.files[0]) {
+        console.log(target.files[0])
+
+        if (!(target.files[0].type === 'image/jpeg' || target.files[0].type === 'image/jpg' || target.files[0].type === 'image/png')) {
+            setError("The chosed file is not valid as type")
+        }
+
+        if (target.files[0].size > 1000000) {
+            setError('The size should be under 1 mgbyte')
+        }
+
+        else if (target.files[0]) {
+            setError("")
             setPhoto(target.files[0])
         }
     }
@@ -34,9 +46,17 @@ const ProfilePicture = () => {
                     <FormInput
                         id='file'
                         type="file"
-                        label='Choose your photo'
+                        accept=".png, .jpg, .jpeg"
+                        label='Choose your Photo'
                         className='file-input'
-                        onChange={handleChange} />
+                        onChange={handleChange}
+
+                    />
+                    {error ?
+                        <p className='error'>{error}</p>
+                        :
+                        <p className='info'>{photo?.name || 'Supported files: jpeg, jpg, png'}</p>
+                    }
                     <Button type='secondary' onClick={handleClick} disabled={loading !== 'idle' || !photo}>{loading === 'uploading' ? 'Uploading...' : 'Upload'}</Button>
                 </div>
             </Modal.Content>
